216a5e28f2cc6e51c0063a78fe62a71af88bb93b
Alvaro Garcia <89421445+alvaro17f@users.noreply.github.com>
2023-02-13 14:18:50 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# GUESS NUMBER
+This is a very simple game.\
+You must guess a number between 0 and 100.\
+Built in rust.
---
+++ src/main.rs
@@ -0,0 +1,59 @@
+use std::{io::stdin, process};
+
+use rand::Rng;
+use text_io::read;
+
+fn main() {
+    loop {
+        let number: u32 = randomize();
+        let mut count: u32 = 0;
+        print!("\x1B[2J\x1B[1;1H");
+        println!("WELCOME TO... GUESS THE NUMBER");
+        println!();
+        println!("Please, insert a number between 0 and 100:");
+        println!();
+        loop {
+            let guess: u32 = read!();
+            count += 1;
+            if guess == number {
+                print!("\x1B[2J\x1B[1;1H");
+                println!();
+                println!("WINNER HERE");
+                println!("{count} trials");
+                break;
+            }
+            if guess > number {
+                print!("\x1B[2J\x1B[1;1H");
+                println!("WELCOME TO... GUESS THE NUMBER");
+                println!();
+                println!("Please, insert a number in order to guess the mine:");
+                println!();
+                println!("your number {} is bigger", guess);
+                println!("Try again:");
+                println!();
+            } else {
+                print!("\x1B[2J\x1B[1;1H");
+                println!("WELCOME TO... GUESS THE NUMBER");
+                println!();
+                println!("Please, insert a number in order to guess the mine:");
+                println!();
+                println!("your number {} is smaller", guess);
+                println!("Try again:");
+                println!();
+            }
+        }
+        println!("Would you like to try again? (y/N): ");
+        let prompt: &mut String = &mut String::new();
+        stdin().read_line(prompt).expect("failed to read line");
+        if let "y\n" | "Y\n" | "yes\n" | "Yes\n" | "YES\n" | "\n" = prompt.as_str() {
+            continue;
+        } else {
+            process::exit(1);
+        } 
+    }
+}
+
+fn randomize() -> u32 {
+    let mut rng = rand::thread_rng();
+    rng.gen_range(0..=100)
+}
